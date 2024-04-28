04aebe676236262b133008d20bfaad8ef9652aa9
mitou <xmitoux@gmail.com>
2024-04-28 09:07:10 +0900

    ESLintルールをフロントエンドと合わせた

---
+++ .eslintrc.cjs
@@ -0,0 +1,43 @@
+module.exports = {
+    env: {
+        node: true,
+        jest: true,
+        es2021: true,
+    },
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        parser: '@typescript-eslint/parser',
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint'],
+    root: true,
+    rules: {
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        'no-console': ['warn', { allow: ['warn', 'error'] }],
+        'array-callback-return': ['error', { checkForEach: true }],
+        '@typescript-eslint/no-unused-vars': [
+            'warn',
+            {
+                argsIgnorePattern: '^_',
+                varsIgnorePattern: '^_',
+                caughtErrorsIgnorePattern: '^_',
+                destructuredArrayIgnorePattern: '^_',
+            },
+        ],
+    },
+};
