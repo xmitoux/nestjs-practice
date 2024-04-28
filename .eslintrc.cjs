7a1af139f5b3ef682313fe621cca9b5b406f7d92
mitou <xmitoux@gmail.com>
2024-04-28 23:27:48 +0900

    不要なESLint設定を削除

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,10 +24,6 @@ module.exports = {
     plugins: ['@typescript-eslint'],
     root: true,
     rules: {
-        '@typescript-eslint/interface-name-prefix': 'off',
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
         'array-callback-return': ['error', { checkForEach: true }],
         '@typescript-eslint/no-unused-vars': [
             'warn',
