0c659e988b1a5a0a852ba12052324a250740d719
mitou <xmitoux@gmail.com>
2024-05-09 21:50:36 +0900

    fix: ユーザ系APIが失敗する不具合を修正
    postsと紐づけないサービスがentityのコンストラクタで失敗していた

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -6,7 +6,10 @@ import { PostEntity } from '@/modules/posts/entities/post.entity';
 export class UserEntity implements User {
     constructor({ posts, ...data }: Partial<UserEntity>) {
         Object.assign(this, data);
-        this.posts = posts!.map((post) => new PostEntity(post));
+
+        if (posts) {
+            this.posts = posts.map((post) => new PostEntity(post));
+        }
     }
 
     id: number;
